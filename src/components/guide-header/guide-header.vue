7ae1bc53f72a88cb268c7be9700e1de4b9d0ddd1
lixin <lixin@jkweixin.com>
2018-09-18 19:45:38 +0800

    完成首页组件top，合并分支

--- src/components/guide-header/guide-header.vue
+++ src/components/guide-header/guide-header.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="guide-header">
-    <section class="log-wrapper">
+    <section class="logo-wrapper">
       <img class="img" mode="aspectFill" :src="url" alt="">
       <button class="share-btn">
-        <img class="share-icon" :src="imageUrl + '/zd-image/1.1/icon-share@2x.png'" alt="">
+        <img class="share-icon" v-if="imageUrl" :src="imageUrl + '/zd-image/1.1/icon-share_dg@2x.png'" alt="">
         <div class="share-txt">分享</div>
       </button>
     </section>
     <section class="content">
-
+      <article class="top">
+        <div class="avatar-wrapper">
+          <img class="avatar" mode="aspectFill" :src="url" alt="">
+        </div>
+        <div class="name">刘辉</div>
+        <div class="detail">
+          <div class="position">美容导师</div>
+          <div class="company">广州国颐堂养发SPA馆</div>
+        </div>
+      </article>
+      <div class="down">"黑色给了我黑色的眼睛我却用它来寻找光明"</div>
     </section>
   </div>
 </template>
@@ -33,7 +43,7 @@
 
   .guide-header
     position: relative
-    .log-wrapper
+    .logo-wrapper
       position: relative
       height: 75vw
       .img
@@ -54,4 +64,48 @@
         .share-txt
           margin-left: 5px
           font-size: $font-size-14
+          color: $color-FFFFFF
+    .content
+      padding: 0 15px
+      .top
+        layout(row, block, nowrap)
+        position: relative
+        padding-top: 15px
+        .avatar-wrapper
+          position: absolute
+          top: -20px
+          left: 0
+          width: 75px
+          height: 75px
+          border-radius: 50%
+          background: $color-FFFFFF
+          layout()
+          justify-content: center
+          align-items: center
+          .avatar
+            width: 95%
+            height: 95%
+            border-radius: 50%
+        .name
+          margin: 0 6px 0 90px
+          font-family: $font-family-medium
+          font-size: 25px
+          color: $color-1F1F1F
+          letter-spacing: 0.75px
+          line-height: 1.2
+        .detail
+          flex: 1
+          font-size: $font-size-12
+          color: $color-6E6E6E
+          letter-spacing: 0.26px
+          line-height: 1.2
+          .position
+            margin-bottom: 2px
+      .down
+        padding: 26.5px 0 35px
+        font-size: $font-size-16
+        color: #1F1F1F
+        letter-spacing: 0.34px
+        line-height: 1.2
+        word-break: break-all
 </style>
--- src/pages/guide/guide.vue
+++ src/pages/guide/guide.vue
@@ -1,15 +1,18 @@
 <template>
   <article class="guide">
     <guide-header></guide-header>
+    <guide-active></guide-active>
   </article>
 </template>
 
 <script type="text/ecmascript-6">
   import GuideHeader from 'components/guide-header/guide-header'
+  import GuideActive from 'components/guide-active/guide-active'
 
   export default {
     components: {
-      GuideHeader
+      GuideHeader,
+      GuideActive
     },
     data() {
       return {}
