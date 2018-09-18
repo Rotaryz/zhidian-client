3136baead683d0ba22871a513dbd3962558a1fb5
lixin <lixin@jkweixin.com>
2018-09-18 21:29:51 +0800

    导购首页

---
+++ src/components/shop-header/shop-header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="shop-header">
+    <swiper class="slide-box">
+      <swiper-item class="slide-item">
+        <img class="item" mode="aspectFill">
+        <img class="item">
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+        dataArray: [1, 1, 1]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/private"
+  .shop-header
+    padding: 0 15px
+    .slide-box
+      height: 33.4vw
+      width: 100vw
+      .slide-item
+        height: 33.4vw
+        width: 100vw
+        layout(row, block, nowrap)
+        justify-content: space-between
+        .item
+          height: 100%
+          width: 48.55%
+          background fuchsia
+
+</style>
--- src/pages/shop/shop.vue
+++ src/pages/shop/shop.vue
@@ -1,18 +1,24 @@
 <template>
-  <article>
+  <article class="shop">
+    <shop-header></shop-header>
   </article>
 </template>
 
 <script type="text/ecmascript-6">
+  import ShopHeader from 'components/shop-header/shop-header'
+
   export default {
+    components: {
+      ShopHeader
+    },
     data() {
-      return {
-      }
+      return {}
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/private"
-
+  .shop
+    min-height: 100vh
 </style>
