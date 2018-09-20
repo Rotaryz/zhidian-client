cc170083c389911efc1b1bda03a9fed06618becb
lixin <lixin@jkweixin.com>
2018-09-20 16:26:55 +0800

    切店接口

--- src/store/getters.js
+++ src/store/getters.js
@@ -5,3 +5,9 @@ export const targetPage = state => state.targetPage
 export const appShowType = state => state.appShowType
 
 export const orderResultMsg = state => state.orderResult // 订单结果页店铺信息
+
+export const nowChat = state => state.nowChat
+
+export const imLogin = state => state.imLogin
+
+export const currentMsg = state => state.currentMsg
--- src/store/state.js
+++ src/store/state.js
@@ -2,7 +2,11 @@ const state = {
   role: null,
   targetPage: null, // 目标页面，用于登录授权后跳转
   orderResult: {}, // 订单支付结果页
-  appShowType: false // app onShow不执行方法
+  appShowType: false, // app onShow不执行方法
+  nowChat: [], // 当前的聊天消息
+  currentMsg: null, // 当前消息对象（名片对象）没有则为null， employeeId 员工id、flowId 流id、nickName 员工姓名、avatar 员工头像, account 员工的IM账号
+  imLogin: false // im的登录态
+
 }
 
 export default state
