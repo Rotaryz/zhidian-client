2010f8e868a00f006860f0bfd13b24877b133398
jtr <jtr354@foxmail.com>
2018-12-21 09:45:28 +0800

    配置工具优化

--- src/utils/env-clear.js
+++ src/utils/env-clear.js
@@ -1,6 +1,6 @@
 import wx from 'wx'
 import APP_CONFIG from './_app-config'
-const APP_ENV = process.env.NODE_ENV
+// const APP_ENV = process.env.NODE_ENV
 export function envClear() {
   const env = APP_CONFIG.env + '：' + APP_CONFIG.api
   const app = APP_CONFIG.app
@@ -13,10 +13,10 @@ export function envClear() {
   console.warn('环境：' + env)
 }
 // 检查生产环境是否配置正确
-function checkVersion() {
-  if (APP_CONFIG.env !== APP_ENV && APP_ENV === 'production') {
-    throw new Error('生产环境配置不一致')
-  }
-}
-checkVersion()
+// function checkVersion() {
+//   if (APP_CONFIG.env !== APP_ENV && APP_ENV === 'production') {
+//     throw new Error('生产环境配置不一致')
+//   }
+// }
+// checkVersion()
 envClear()
